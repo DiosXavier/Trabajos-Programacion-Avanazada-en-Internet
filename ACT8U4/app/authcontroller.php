4be6c3f8872f822a5244006f72ed5008118c8133
Xavier Juarez <62969102+DiosXavier@users.noreply.github.com>
2022-09-27 22:32:48 -0600

    Add files via upload

---
+++ ACT8U4/app/authcontroller.php
@@ -0,0 +1,52 @@
+<?php
+
+if(isset($_POST['accion'])){
+    switch($_POST['accion']){
+        case 'access':
+            $authController =new authController();
+            $email=strip_tags($_POST['email']);
+            $password=strip_tags($_POST['password']);
+            $authController -> login($email,$password);
+    }
+}
+
+class AuthController{
+    public function login($email, $password){
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response=json_decode($response);
+        var_dump($response);
+
+        if(isset($response->code) && $response->code>0)
+        {
+            session_start();
+            $SESSION['name']=$response->data->name;
+            $SESSION['lastname']=$response->data->lastname;
+            $SESSION['avatar']=$response->data->avatar;
+            $SESSION['token']=$response->data->token;
+            var_dump($response);
+            header("location: ../productos/index.php");
+        }else{
+            var_dump($response);
+            header("location: ../?error=true");
+        }
+
+
+    }
+}
+
+#'jeju_19@alu.uabcs.mx'
+#O338lXPk!5k8I6
