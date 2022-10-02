272c7455d388e23976c876b615f0d24bf0a60027
Xavier Juarez <62969102+DiosXavier@users.noreply.github.com>
2022-10-02 15:38:52 -0600

    Add files via upload

---
+++ ACT18U4/app/ProdController.php
@@ -0,0 +1,96 @@
+<?php session_start(); ?>
+
+<?php
+
+if(isset($_POST['action'])){
+    switch($_POST['action']){
+        case 'create':
+            $name = $_POST['name'];
+            $slug = $_POST['slug'];
+            $description= $_POST['description'];
+            $features = $_POST['features'];
+            $brand_id = $_POST['brand_id'];
+
+            $p = new ProdController();
+
+            $imagen = $p->consImg($_FILES['uploadedfile']);
+
+            $p->create($name, $slug, $description, $features, $brand_id,$imagen);   
+            break;
+        
+    }
+}
+
+
+
+class ProdController{
+
+    public function consImg($arch){
+        $target_path  = '../public/image';
+        $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+            echo "El archivo ".  basename( $_FILES['uploadedfile']['name']). 
+            " ha sido subido";
+        } else{
+            echo "Ha ocurrido un error, trate de nuevo!";
+        }
+        return $target_path;
+        
+    }
+    
+    public function getTodo(){
+        $curl = curl_init();
+        $token = $_SESSION['token'];
+        #echo $token;
+        curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer ".$token
+    ),
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+    }
+
+    public function create($name, $slug, $description, $features, $brand_id, $imagen){
+
+        $token = $_SESSION['token'];    
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('name' => $name,'slug' => $slug,'description' =>$description,'features' => $features,'brand_id' => $brand_id,'cover'=> NEW CURLFile($imagen)),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$token
+        ),
+    ));
+    
+    
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    unlink($imagen);
+    header('Location: ../productos/index.php?success=true');
+    
+    }
+    }
+
+#'jeju_19@alu.uabcs.mx'
+#O338lXPk!5k8I6
+?>
+
